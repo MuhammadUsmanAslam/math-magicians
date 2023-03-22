b1494d3b45d823e66002ac1fc2e10ade6df255e8
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-03-22 20:16:20 +0500

    Add calculator logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,21 @@
 import './Calculator.css';
 import CalculatorRow from './CalculatorRow.js';
+import {useState } from 'react';
 
 function Calculator() {
     const RowData = [["AC", "+/-", "%", "÷"], ["7", "8", "9", "x"], ["4", "5", "6", "-"], ["1", "2", "3", "+"], ["0", ".", "="]];
+
+    const [inputData, setInputData] = useState({
+        total: null,
+        next: null,
+        operation: null,
+    });
+
     return (
         <div className="calculator">
-            <input className='calculator__input' type="text" defaultValue={0} />
+            <div className='calculator__input'>{inputData.total}{inputData.operation}{inputData.next}</div>
             {RowData.map((RowDataItem, index) => {
-                return <CalculatorRow key={index} data={RowDataItem} />
+                return <CalculatorRow key={index} inputData={inputData} setInputData={setInputData} data={RowDataItem} />
             })}
         </div >
     );
